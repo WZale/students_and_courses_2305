718e130b08c1c417b0becb9d84cd12c4503327f0
William Zale <williamzale@gmail.com>
2023-05-24 11:14:38 -0400

    feat: Add class structure and methods for log_score(score) and grade

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,19 @@
 class Student
- 
+  attr_reader :name,
+              :age,
+              :scores
+
+  def initialize((student_info))
+    @name = student_info[:name]
+    @age = student_info[:age]
+    @scores = []
+  end
+
+  def log_score(score)
+    @scores << score
+  end
+
+  def grade
+    @scores.sum / @scores.length.to_f  
+  end
 end
