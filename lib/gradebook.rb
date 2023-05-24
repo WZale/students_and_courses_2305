3da5b9f0e22d50207f029d444f58458b4d9dc64e
William Zale <williamzale@gmail.com>
2023-05-24 12:35:54 -0400

    feat: Add class structure and methods for add_course, list_all_students and stuents_below_threshold

--- lib/gradebook.rb
+++ lib/gradebook.rb
@@ -1,3 +1,31 @@
 class Gradebook
+  attr_reader :instructor,
+              :courses
+
+  def initialize(instructor)
+    @instructor = instructor
+    @courses = []
+  end
+
+  def add_course(course)
+    @courses << course
+  end
+
+  def list_all_students
+    students_by_course = {}
+    @courses.each do |course|
+      students_by_course[course] = course.students
+    end
+    students_by_course
+  end
+
+
+  def students_below_threshold(score)
+    @courses.flat_map do |course|
+      course.students.find_all do |student|
+        student.grade < score
+      end
+    end
+  end
   
 end
